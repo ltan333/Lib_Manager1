3648a57b7723b8272b6a518e8f3a0767b6fd52bb
An Lee <truongan147258@gmail.com>
2021-03-13 22:47:09 +0700

    thêm 1 sách, nhìu sách, tự cập nhật số lượng sách

--- src/library/manager/BookManager.java
+++ src/library/manager/BookManager.java
@@ -12,12 +12,32 @@ import java.util.ArrayList;
  * @author ACER
  */
 public class BookManager {
-    ArrayList<Book> bookList = new ArrayList<>();
-    public int numberOfadmin;
+    public ArrayList<Book> bookList = new ArrayList<>();
+    public int numberOfBooks;
+    public Books books = new Books();
     
-    public void addFixdelate(){
+    public void inputs() {
+        int flag = 0;
+        books.inputs();
+        for (Book element : bookList) {
+            if(element.getBookname().contains(books.books.get(0).getBookname())) {
+                flag = 1;
+            }
+        }
+        if (flag == 0) {
+            bookList.addAll(books.books);
+        }
+        else {
+            for (int i = 0; i < bookList.size(); i++) {
+                bookList.addAll(books.books);
+                if(bookList.get(i).getBookname().contains(books.books.get(0).getBookname())) {
+                    bookList.get(i).setAmount(bookList.get(i).getAmount()+books.getNumberOfBooks());
+                }
+            }
+        }
         
     }
+    
     
     public void findByname(){
         
--- src/library/manager/Books.java
+++ src/library/manager/Books.java
@@ -5,10 +5,43 @@
  */
 package library.manager;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author DELL
  */
 public class Books extends Book{
+    public int numberOfBooks;
+    public ArrayList<Book> books;
+
+    public int getNumberOfBooks() {
+        return numberOfBooks;
+    }
+
+    public void setNumberOfBooks(int numberOfBooks) {
+        this.numberOfBooks = numberOfBooks;
+    }
+    Scanner scan = new Scanner(System.in);
     
+    public void inputs() {
+        super.input();
+        System.out.print("- Số lượng: "); 
+        this.setNumberOfBooks(scan.nextInt());
+        scan.nextLine();
+        new ArrayList<>(this.getNumberOfBooks());
+        for (int i = 0; i < this.getNumberOfBooks(); i++) {
+            Book b = new Book();
+            b.setBookname(this.getBookname());
+            b.setAuthor(this.getAuthor());
+            b.setCondition(this.getCondition());
+            b.setKind(this.getKind());
+            b.setMoney(this.getMoney());
+            b.setAmount(this.getNumberOfBooks());
+            books.add(b);
+        }
+        
+        
+    }
 }
